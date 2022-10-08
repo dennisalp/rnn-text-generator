98ccb5692460d1958f19a9919854599373c1bd02
Dennis Alp <me@dennisalp.com>
2022-10-09 00:25:05 +0200

    support pre-trained models

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     
     input_path = os.path.join('dat', 'astrophysics.txt')
     pth = 'mod'
-    init_str = 'He '
+    init_str = 'what?'
     len_out_str = 2000
     seq_length = 100
     batch_size = 64
@@ -17,9 +17,12 @@ def main():
     epochs = 30
 
     dataset, ids2chars, chars2ids = read_data(input_path, batch_size, seq_length, buffer_size)
-    model = train_mod(dataset, epochs, embedding_dim, rnn_units, ids2chars, chars2ids)
-    model.save(pth)
-    model = load_model(pth)
+    if os.path.isdir(pth):
+        model = load_model(pth)
+    else:
+        model = train_mod(dataset, epochs, embedding_dim, rnn_units, ids2chars, chars2ids)
+        model.save(pth)
+    
     gen_txt(model, init_str, len_out_str, ids2chars, chars2ids, temperature=1.0)
     st()
     gen_txt(model, init_str, len_out_str, ids2chars, chars2ids, temperature=1.0)
